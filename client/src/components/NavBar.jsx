e65a60fff1f4a97f01ebc310c7c2c43c8c116deb
ZIYAD K <80669764+ZiyadKadekara@users.noreply.github.com>
2023-03-30 11:25:36 +0530

    Added Nav bar

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -1,8 +1,37 @@
-import react  from 'react';
-
+import React  from 'react';
+import Logo from "../img/logo.png";
+import {Link} from 'react-router-dom';
 const NavBar = () => {
     return (
-        <div>NavBar</div>
+        <div className='navbar'>
+            <div className='container'>
+                <div className='logo'>
+                    <img src={Logo} alt=""/>
+                </div>
+                <div className='links'>
+                    <Link className='link' to ="/?cat=research">
+                        <h6>Research</h6>
+                    </Link>
+                    <Link className='link' to ="/?cat=Competition">
+                        <h6>Competition</h6>
+                    </Link>
+                    <Link className='link' to ="/?cat=Internship">
+                        <h6>Internship</h6>
+                    </Link>
+                    <Link className='link' to ="/?cat=sports">
+                        <h6>Sports</h6>
+                    </Link>
+                    <Link className='link' to ="/?cat=arts">
+                        <h6>Arts</h6>
+                    </Link>
+                    <span>John</span>
+                    <span>Logout</span>
+                    <span className='write'>
+                        <Link className='link' to ="/write">Add</Link>
+                    </span>
+                </div>
+            </div>
+        </div>
     )
 }
 
